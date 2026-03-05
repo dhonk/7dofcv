113bb85bf4dcbbfe4268f2e5368de0dca2567183
dhonk <dhong12@ucsc.edu>
2026-03-05 00:14:34 -0800

    added init config file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+python3.14 -m venv ./venv
+pip install -r requirements.txt
+wget "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task" 
+wget "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task"
