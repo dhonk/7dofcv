01c0bb758535dfd48841d36649383a1f7da538a3
dhonk <dhong12@ucsc.edu>
2026-03-05 01:40:10 -0800

    frames gotten

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 python3.14 -m venv ./venv
+source ./venv/bin/activate
 pip install -r requirements.txt
 wget "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task" 
 wget "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task"
